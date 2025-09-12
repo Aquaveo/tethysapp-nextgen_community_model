5b4a8392c3f21e56ef47184c721ab456c44b988a
James Dolinar <jdolinar@aquaveo.com>
2025-09-11 19:31:27 -0600

    Replace default app icon with custom image

--- tethysapp/nextgen_community_model/app.py
+++ tethysapp/nextgen_community_model/app.py
@@ -9,7 +9,7 @@ class App(TethysAppBase):
     description = ''
     package = 'nextgen_community_model'  # WARNING: Do not change this value
     index = 'map_view'
-    icon = f'{package}/images/icon.gif'
+    icon = f'{package}/images/icon.png'
     root_url = 'nextgen-community-model'
     color = '#192a56'
     tags = ''
